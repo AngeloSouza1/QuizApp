8a3f9d1288161be4ef568f22886ceb2698533a89
AngeloSouza1 <angeloafdesouza@gmail.com>
2024-08-09 21:10:04 -0300

    chore: ajuste tela login [home, user, adm]

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  # Redireciona para o dashboard após o login
+  def after_sign_in_path_for(resource)
+    if resource.is_a?(Admin)
+      admin_root_path  # Redireciona para o dashboard do admin
+    else
+      user_dashboard_path  # Redireciona para o dashboard do usuário
+    end
+  end
 end
