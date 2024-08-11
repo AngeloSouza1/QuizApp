3315f705dfb99e318e57b146a780b61e15516bb4
AngeloSouza1 <angeloafdesouza@gmail.com>
2024-08-11 15:24:38 -0300

    chore: adição da view dashboard Admin [utimos quiazzes respondidos]

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,8 +1,17 @@
 class DashboardsController < ApplicationController
   before_action :authenticate_user! # Certifica que o usuário está autenticado antes de acessar o dashboard
+
   def show
+    # Certifica que o usuário está autenticado
     @user_level = current_user.user_level if current_user.respond_to?(:user_level)
-    @recent_answers = current_user.answers.includes(question: :quiz).order(created_at: :desc).limit(5)
-    @quizzes = Quiz.all  # Certifique-se de que @quizzes é inicializado
+
+    # Busca as respostas mais recentes do usuário atual
+        @recent_answers = current_user.answers
+        .joins(:question)
+        .select('DISTINCT ON (questions.quiz_id) answers.*')
+        .order('questions.quiz_id, answers.created_at DESC')
+        .limit(5)
+    # Busca todos os quizzes disponíveis
+    @quizzes = Quiz.all
   end
 end
