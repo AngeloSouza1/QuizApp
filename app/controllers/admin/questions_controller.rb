1e2e95afa0176230212e6fc260e2654e2c735fb8
AngeloSouza1 <angeloafdesouza@gmail.com>
2024-08-11 11:19:47 -0300

    chore: adicção da view dashboard Admin [inclusao pergunta e respostas]

--- app/controllers/admin/questions_controller.rb
+++ app/controllers/admin/questions_controller.rb
@@ -1,7 +1,7 @@
 # app/controllers/admin/questions_controller.rb
 class Admin::QuestionsController < ApplicationController
   before_action :authenticate_admin!
-  before_action :set_quiz
+  before_action :set_quiz, only: [:new, :create, :edit, :update, :destroy]
   before_action :set_question, only: [:show, :edit, :update, :destroy]
 
   def index
@@ -14,13 +14,17 @@ class Admin::QuestionsController < ApplicationController
   def new
     @quiz = Quiz.find(params[:quiz_id])
     @question = @quiz.questions.build
-    @question.answers.build # Adiciona uma resposta em branco
+    5.times { @question.answers.build } # Sempre cria 5 campos de resposta em branco
   end
 
   def create
     @question = @quiz.questions.build(question_params)
+    @question.answers.each do |answer|
+      answer.user_id = current_admin.id
+    end
+    
     if @question.save
-      redirect_to admin_quiz_questions_path(@quiz), notice: 'Pergunta criada com sucesso.'
+      redirect_to admin_quiz_path(@quiz), notice: 'Pergunta criada com sucesso.'
     else
       render :new
     end
@@ -29,28 +33,21 @@ class Admin::QuestionsController < ApplicationController
   def edit
     @quiz = Quiz.find(params[:quiz_id])
     @question = @quiz.questions.find(params[:id])
-    
-    # Inicializa pelo menos uma resposta se nenhuma estiver presente
-    @question.answers.build if @question.answers.empty?
+    (5 - @question.answers.size).times { @question.answers.build } # Adiciona campos de resposta para totalizar 5
   end
-  
+
   def update
-    @quiz = Quiz.find(params[:quiz_id])
-    @question = @quiz.questions.find(params[:id])
-  
-    params[:question][:answers_attributes].each do |key, answer_params|
-      answer_params[:user_id] = current_admin.id if answer_params[:user_id].blank?
-    end if params[:question][:answers_attributes]
-  
-    if @question.update(question_params)
+    @question.assign_attributes(question_params)
+    @question.answers.each do |answer|
+      answer.user_id = current_admin.id if answer.user_id.nil?
+    end
+
+    if @question.save
       redirect_to admin_quiz_path(@quiz), notice: 'Pergunta atualizada com sucesso.'
     else
-      render :edit, status: :unprocessable_entity
+      render :edit
     end
   end
-  
-  
-
 
   def destroy
     @question.destroy
@@ -67,12 +64,7 @@ class Admin::QuestionsController < ApplicationController
     @question = @quiz.questions.find(params[:id])
   end
 
- 
-def question_params
-  params.require(:question).permit(:content, answers_attributes: [:id, :content, :correct, :user_id, :_destroy])
-end
-
-
-
-
+  def question_params
+    params.require(:question).permit(:content, answers_attributes: [:id, :content, :correct, :user_id, :_destroy])
+  end
 end
--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,10 +1,8 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
-
-
 import "jquery"
 
 
-
+console.log("JavaScript carregado e funcionando!");
 
