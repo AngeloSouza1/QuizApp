0432438efd9e65cec8cf5a8753b3b6954f076c0c
AngeloSouza1 <angeloafdesouza@gmail.com>
2024-08-12 12:25:19 -0300

    chore: adição da view dashboard Users [Atualizacao quizz atualizdos ]

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -8,43 +8,44 @@ class QuizzesController < ApplicationController
   end
 
   def show
+    @quiz = Quiz.find(params[:id])
     @user_level = current_user.user_level
     @recent_answers = current_user.answers.includes(question: :quiz).order(created_at: :desc).limit(5)
     @questions = @quiz.questions.includes(:answers)
   end
 
-  def answer
-    correct_count = 0
-  
-    if params[:responses].present?
-      params[:responses].each do |question_id, answer_id|
-        question = Question.find(question_id)
-        selected_answer = question.answers.find(answer_id)
-  
-        # Cria uma nova resposta para o usuário
-        user_answer = current_user.answers.create(
-          question: question,
-          content: selected_answer.content,
-          correct: selected_answer.correct
-        )
-  
-        if user_answer.persisted?
-          correct_count += 1 if selected_answer.correct?
-          # Atualiza a pontuação do usuário
-          current_user.user_level.increment!(:points, 10) if selected_answer.correct?
-        else
-          flash[:alert] = "Ocorreu um erro ao salvar suas respostas. Por favor, tente novamente."
-          render :show and return
-        end
+
+def answer
+  correct_count = 0
+
+  if params[:responses].present?
+    params[:responses].each do |question_id, answer_id|
+      question = Question.find(question_id)
+      selected_answer = question.answers.find(answer_id)
+
+      # Verifica se já existe uma resposta do usuário para essa pergunta
+      user_answer = current_user.answers.find_or_initialize_by(question: question)
+
+      # Atualiza a resposta existente com o novo conteúdo e status
+      user_answer.update(
+        content: selected_answer.content,
+        correct: selected_answer.correct
+      )
+
+      # Atualiza a pontuação do usuário, se a resposta estiver correta
+      if selected_answer.correct?
+        correct_count += 1
+        current_user.user_level.increment!(:points, 10)
       end
-    else
-      flash[:alert] = "Nenhuma resposta foi submetida."
-      render :show and return
     end
-  
-    redirect_to quiz_path(@quiz), notice: "#{correct_count} respostas corretas!"
+  else
+    flash[:alert] = "Nenhuma resposta foi submetida."
+    render :show and return
   end
-  
+
+  redirect_to quiz_path(@quiz), notice: "#{correct_count} respostas corretas!"
+end
+
   
 
   private
