153751c6a6af065f5a938ca94e5702d64a17b09f
AngeloSouza1 <angeloafdesouza@gmail.com>
2024-08-17 14:32:52 -0300

    chore[usr]: ajuste layout usuarios, imagem e home

--- config/application.rb
+++ config/application.rb
@@ -11,18 +11,17 @@ module QuizApp
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.1
 
+    # Configuration for the application, engines, and railties goes here.
+    config.time_zone = 'America/Sao_Paulo'
+    config.active_record.default_timezone = :local
+    config.i18n.default_locale = :pt
+
+    # Add any custom paths to autoload_paths here
+    # config.autoload_paths += %W(#{config.root}/lib)
+
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
-    config.autoload_lib(ignore: %w(assets tasks))
-
-    # Configuration for the application, engines, and railties goes here.
-    #
-    # These settings can be overridden in specific environments using the files
-    # in config/environments, which are processed later.
-    #
-    config.time_zone = 'America/Sao_Paulo'
-    config.active_record.default_timezone = :local
-    # config.eager_load_paths << Rails.root.join("extras")
+    config.autoload_paths -= %W(#{config.root}/lib/assets #{config.root}/lib/tasks)
   end
 end
---
+++ config/initializers/i18n.rb
@@ -0,0 +1,2 @@
+I18n.available_locales = [:en, :pt] # Adicione outros locales se necessário
+I18n.default_locale = :pt
