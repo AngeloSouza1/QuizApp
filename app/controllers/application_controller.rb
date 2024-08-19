066c995dcd8ca38c6fd1d25b7221bc79b2a02598
AngeloSouza1 <angeloafdesouza@gmail.com>
2024-08-19 09:14:57 -0300

    chore[user]: ajuste gerenciamento viewsn autenticacaodevisecarrossel

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
   # Redireciona para o dashboard após o login
   def after_sign_in_path_for(resource)
     if resource.is_a?(Admin)
@@ -6,5 +8,13 @@ class ApplicationController < ActionController::Base
     else
       user_dashboard_path  # Redireciona para o dashboard do usuário
     end
+  end
+
+  protected
+
+  # Permite o campo nome durante o cadastro e a atualização da conta
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nome])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:nome])
   end
 end
